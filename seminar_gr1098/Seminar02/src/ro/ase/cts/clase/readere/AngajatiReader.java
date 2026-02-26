789d167b12fce581a1640f8c00923c2b68a28c38
Padureanu Andreea <padureanuandreea150@gmail.com>
2026-02-26 19:00:38 +0200

    seminar 2 cts - principii clean code

---
+++ seminar_gr1098/Seminar02/src/ro/ase/cts/clase/readere/AngajatiReader.java
@@ -0,0 +1,30 @@
+package ro.ase.cts.clase.readere;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AngajatiReader extends AplicantReader {
+
+    @Override
+    public List<Aplicant> readAplicant(String file) throws FileNotFoundException {
+        Scanner input2 = new Scanner(new File(file));
+        input2.useDelimiter(",");
+        List<Aplicant> angajati = new ArrayList<Aplicant>();
+
+        while (input2.hasNext()) {
+            Angajat a = new Angajat();
+            super.readAplicant(input2,a);
+            int salariu = input2.nextInt();
+            String ocupatie = input2.next();
+            angajati.add(a);
+        }
+        input2.close();
+        return angajati;
+    }
+}
